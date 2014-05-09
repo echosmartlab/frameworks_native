a99050edaf4ae22db3b79d8fdc89f14c18c64317
codewalker <codewalker@hardkernel.com>
2014-05-09 20:50:24 +0900

    samsung patch

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -74,6 +74,9 @@
 
 #include "RenderEngine/RenderEngine.h"
 #include <cutils/compiler.h>
+#if defined(BOARD_USES_HDMI)
+#include "SecTVOutService.h"
+#endif
 
 #define DISPLAY_COUNT       1
 
@@ -175,6 +178,11 @@ SurfaceFlinger::SurfaceFlinger()
     }
     ALOGI_IF(mDebugRegion, "showupdates enabled");
     ALOGI_IF(mDebugDDMS, "DDMS debugging enabled");
+
+    #if defined(BOARD_USES_HDMI)
+    LOGD(">>> Run hdmi service");
+    android::SecTVOutService::instantiate();
+    #endif
 }
 
 void SurfaceFlinger::onFirstRef()
