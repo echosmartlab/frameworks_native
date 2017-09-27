ae4d1627ae967386d91cdd28e0485f27b57cf093
codewalker <codewalker@hardkernel.com>
2017-09-27 11:05:24 +0900

    Revert "box product do not have sensor ,skip SensorService init."
    This reverts commit dd6642b237a5509109510ecdfa18492bd4b03403.
    
    Change-Id: Id709468e3a70058b00b5d29a89cbff449a4b3708

--- services/sensorservice/SensorService.cpp
+++ services/sensorservice/SensorService.cpp
@@ -130,14 +130,6 @@ void SensorService::enableSchedFifoMode() {
 
 void SensorService::onFirstRef() {
     ALOGD("nuSensorService starting...");
-    char value[PROPERTY_VALUE_MAX];
-    property_get("ro.target.product", value, "0");
-    if(!strcmp("box",value))
-    {
-        ALOGD("----box product do not have sensor,skip----");
-        return;
-    }
-
     SensorDevice& dev(SensorDevice::getInstance());
 
     sHmacGlobalKeyIsValid = initializeHmacKey();
